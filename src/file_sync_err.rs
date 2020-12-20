1f8148a44e49cd84bb40a00614fcc95d7265eb6b
Ayomide Bamidele <ayoeze@hotmail.com>
2020-12-20 18:03:30 +0000

    refactored file sync err type

--- src/file_sync_err.rs
+++ src/file_sync_err.rs
@@ -3,10 +3,10 @@ use actix_web::{
 };
 use derive_more::{Display, Error};
 
-#[derive(Debug, Display)]
-enum ErrType {
-    #[display(fmt = "internal error")]
-    InternalError,
+#[derive(Debug, Display, Error)]
+enum FileSyncError {
+    #[display(fmt = "internal error {}", cause)]
+    InternalError { cause: String },
 
     #[display(fmt = "bad request")]
     BadClientData,
@@ -14,36 +14,19 @@ enum ErrType {
     #[display(fmt = "timeout")]
     Timeout,
 }
-#[derive(Debug, Display, Error)]
-#[display(fmt = "{}: {}", _type, message)]
-pub struct FileSyncError {
-    _type: ErrType,
-    message: &'static str,
-
-}
 
 impl error::ResponseError for FileSyncError {
     fn error_response(&self) -> HttpResponse {
-        match self._type {
-            ErrType::InternalError => {
-                HttpResponseBuilder::new(self.status_code())
-                    .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
-                    .body(self.message)
-            }
-            _ => {
-                HttpResponseBuilder::new(self.status_code())
-                .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
-                .body(self.to_string())
-            }
-        }
-        
+        HttpResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .body(self.to_string())
     }
 
     fn status_code(&self) -> StatusCode {
-        match self._type {
-            ErrType::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
-            ErrType::BadClientData => StatusCode::BAD_REQUEST,
-            ErrType::Timeout => StatusCode::GATEWAY_TIMEOUT,
+        match *self {
+            FileSyncError::InternalError{..} => StatusCode::INTERNAL_SERVER_ERROR,
+            FileSyncError::BadClientData => StatusCode::BAD_REQUEST,
+            FileSyncError::Timeout => StatusCode::GATEWAY_TIMEOUT,
         }
     }
 }
